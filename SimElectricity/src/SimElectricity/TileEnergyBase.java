d1c2270843eded3473d034ccf1c13f5f663a4c7b
MiniLight.Aurora <MiniLightAR@Gmail.com>
2013-11-07 22:57:38 +0800

    企划中增加运算思路 代码中增加寻找相邻节点的方法

--- SimElectricity/src/SimElectricity/TileEnergyBase.java
+++ SimElectricity/src/SimElectricity/TileEnergyBase.java
@@ -1,26 +1,58 @@
 package SimElectricity;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import SimElectricity.API.TileAttachEvent;
 import SimElectricity.API.TileDetachEvent;
 
 public abstract class TileEnergyBase extends TileEntity {
-	public boolean isAddedToEnergyNet=false;
-	
+	public boolean isAddedToEnergyNet = false;
+
 	public void updateEntity() {
-		if(!worldObj.isRemote&!isAddedToEnergyNet){
+		if (!worldObj.isRemote & !isAddedToEnergyNet) {
 			MinecraftForge.EVENT_BUS.post(new TileAttachEvent(this));
-			isAddedToEnergyNet=true;
+			isAddedToEnergyNet = true;
 		}
 	}
-	
-    @Override
-    public void invalidate()
-    {
-    	if(!worldObj.isRemote&isAddedToEnergyNet){
+
+	@Override
+	public void invalidate() {
+		if (!worldObj.isRemote & isAddedToEnergyNet) {
 			MinecraftForge.EVENT_BUS.post(new TileDetachEvent(this));
-			isAddedToEnergyNet=false;
-    	}
-    }
+			isAddedToEnergyNet = false;
+		}
+	}
+
+	public TileEnergyBase getNeighboringTileEntity(int direction) {
+		TileEntity result = null;
+		switch (direction) {
+		case 0:
+			result = worldObj.getBlockTileEntity(xCoord - 1, yCoord, zCoord);
+			break;
+		case 1:
+			result = worldObj.getBlockTileEntity(xCoord + 1, yCoord, zCoord);
+			break;
+		case 2:
+			result = worldObj.getBlockTileEntity(xCoord, yCoord - 1, zCoord);
+			break;
+		case 3:
+			result = worldObj.getBlockTileEntity(xCoord, yCoord + 1, zCoord);
+			break;
+		case 4:
+			result = worldObj.getBlockTileEntity(xCoord, yCoord, zCoord - 1);
+			break;
+		case 5:
+			result = worldObj.getBlockTileEntity(xCoord, yCoord, zCoord + 1);
+			break;
+		default:
+			break;
+		}
+		
+		if(result instanceof TileEnergyBase){
+			return (TileEnergyBase) result;
+		}
+		else 
+			return null;
+	}
 }
