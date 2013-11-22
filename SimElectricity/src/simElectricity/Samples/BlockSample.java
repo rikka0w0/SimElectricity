152bcf170a58f956071b7ed1a77ea13b7c8288cd
MiniLight.Aurora <MiniLightAR@Gmail.com>
2013-11-22 13:55:11 +0800

    添加方块图标

--- SimElectricity/src/simElectricity/Samples/BlockSample.java
+++ SimElectricity/src/simElectricity/Samples/BlockSample.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -16,7 +18,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class BlockSample extends BlockContainer {
 	public static final String[] subNames = { "Battery", "Conductor",
 			"Resistor" };
-	private Icon[][] iconBuffer;
+	private Icon[] iconBuffer = new Icon[3];
 
 	// Get TileEntities
 	@Override
@@ -31,6 +33,40 @@ public class BlockSample extends BlockContainer {
 		default:
 			return null;
 		}
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#registerIcons(net.minecraft.client.renderer.texture.IconRegister)
+	 */
+    @SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister par1IconRegister) {
+    	iconBuffer[0] = par1IconRegister.registerIcon("simElectricity:Block_SampleBattery");
+    	iconBuffer[1] = par1IconRegister.registerIcon("simElectricity:Block_SampleConductor");
+    	iconBuffer[2] = par1IconRegister.registerIcon("simElectricity:Block_SampleResistor");
+//		super.registerIcons(par1IconRegister);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#getBlockTexture(net.minecraft.world.IBlockAccess, int, int, int, int)
+	 */
+    @SideOnly(Side.CLIENT)
+	@Override
+	public Icon getBlockTexture(IBlockAccess par1iBlockAccess, int par2,
+			int par3, int par4, int par5) {
+    	int blockMeta = par1iBlockAccess.getBlockMetadata(par2, par3, par4);
+    	return iconBuffer[blockMeta];
+//		return super.getBlockTexture(par1iBlockAccess, par2, par3, par4, par5);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#getIcon(int, int)
+	 */
+    @SideOnly(Side.CLIENT)
+	@Override
+	public Icon getIcon(int par1, int par2) {
+    	return iconBuffer[par2];
+//		return super.getIcon(par1, par2);
 	}
 
 	public BlockSample(int id) {
