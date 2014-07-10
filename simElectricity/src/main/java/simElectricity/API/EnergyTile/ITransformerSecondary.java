216069ab43a87170631f88e9d64edef03838dd4e
Zhou <929513338@qq.com>
2014-07-11 02:08:53 +0800

    Add interfaces for transformer

---
+++ simElectricity/src/main/java/simElectricity/API/EnergyTile/ITransformerSecondary.java
@@ -0,0 +1,6 @@
+package simElectricity.API.EnergyTile;
+
+import simElectricity.EnergyNet.ITransformerComponent;
+
+/** This interface represents the secondary of a transformer */
+public interface ITransformerSecondary extends ITransformerComponent{}
