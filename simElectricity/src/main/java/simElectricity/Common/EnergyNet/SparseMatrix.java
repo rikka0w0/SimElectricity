8a42bfcf80fe86cbd6a8f0ded32609c520edecfb
Rikka0w0 <929513338@qq.com>
2014-09-12 17:48:54 +1000

    www

--- simElectricity/src/main/java/simElectricity/Common/EnergyNet/SparseMatrix.java
+++ simElectricity/src/main/java/simElectricity/Common/EnergyNet/SparseMatrix.java
@@ -9,71 +9,13 @@ import edu.emory.mathcs.csparsej.tdouble.Dcs_util;
 public class SparseMatrix implements MatrixResolver{
 	public static final double EPSILON = (double) 1e-10;
 	
-	public static boolean demo(){
-	    int size = 4; //Size of the square matrix
-	    int nz = 10;  //Number of non zero entries
-	    int[] Ap = {0, 3, 6, 8, 10};
-	    int[] Ai = {1, 3, 0, 1, 3, 2, 2, 0, 3, 1};
-	    double[] Ax= {3.1, 3.5, 4.5, 2.9, 0.4, 1.7, 3.0, 3.2, 1.0, 0.9};
-	    double[] b = {2,2,2,2}; //Right hand side
-	    
-	    Dcs_common.Dcs matrix = Dcs_util.cs_spalloc(size, size, nz, true, false);
-	    matrix.p = Ap;
-	    matrix.i = Ai;
-	    matrix.x = Ax;
-		
-	    boolean result =  Dcs_lusol.cs_lusol(1, matrix, b, 1.0); //Result will be in b
-	    return result;
-	}
-	
-	public static boolean demo2(){
-		MatrixResolver matrix = new SparseMatrix();
-		matrix.newMatrix(4);
-		
-		matrix.pushCoefficient(4.5);
-		matrix.pushCoefficient(3.1);
-		matrix.pushCoefficient(0);
-		matrix.pushCoefficient(3.5);
-		matrix.pushColumn();
-
-		matrix.pushCoefficient(0);
-		matrix.pushCoefficient(2.9);
-		matrix.pushCoefficient(1.7);
-		matrix.pushCoefficient(0.4);
-		matrix.pushColumn();
-
-		matrix.pushCoefficient(3.2);
-		matrix.pushCoefficient(0);
-		matrix.pushCoefficient(3.0);
-		matrix.pushCoefficient(0);
-		matrix.pushColumn();
-		
-		matrix.pushCoefficient(0);
-		matrix.pushCoefficient(0.9);
-		matrix.pushCoefficient(0);
-		matrix.pushCoefficient(1.0);
-		matrix.pushColumn();
-		
-	    //int size = 4; //Size of the square matrix
-	    //int nz = 10;  //Number of non zero entries
-	    //int[] Ap = {0, 3, 6, 8, 10};
-	    //int[] Ai = {1, 3, 0, 1, 3, 2, 2, 0, 3, 1};
-	    //double[] Ax= {3.1, 3.5, 4.5, 2.9, 0.4, 1.7, 3.0, 3.2, 1.0, 0.9};
-	    double[] b = {2,2,2,2}; //Right hand side
-	    
-	    //solution = {0.00211652654477031,0.0787818213886764,0.622023634546417,1.96107942853783}
-	    
-	    matrix.solve(b);
-	    return true;
-	}
-
-	int size;
-	int[] Ap;
-	LinkedList<Integer> AiList;
-	LinkedList<Double> AxList;
+	int size;					//Size of the square matrix
+	int[] Ap;					//Column offset (length = size+1)
+	LinkedList<Integer> AiList;	//Index of entries
+	LinkedList<Double> AxList;	//Value of entries
 	int currentColumn;
 	int currentRow;
-	int nZ;
+	int nZ;						//Total number of non zero elements
 	
 	@Override
 	public void newMatrix(int size) {
@@ -111,6 +53,7 @@ public class SparseMatrix implements MatrixResolver{
 	    Dcs_common.Dcs matrix = Dcs_util.cs_spalloc(size, size, nZ, true, false);
 	    matrix.p = Ap;
 
+	    //Shift the list into array
 	    Integer i = AiList.poll();
 	    int j = 0;
 	    while (i!=null){
