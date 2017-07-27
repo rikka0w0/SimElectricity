8d1527f4d25cc1634ec259343d1363dbc89f839e
MiniLight <MiniLightAR@Gmail.com>
2017-07-27 20:18:08 +0800

    try ic2 implements

--- src/main/java/simelectricity/energybridge/ic2/cable/BlockIc2Cable.java
+++ src/main/java/simelectricity/energybridge/ic2/cable/BlockIc2Cable.java
@@ -4,10 +4,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 import simelectricity.essential.cable.BlockCable;
 import simelectricity.essential.cable.Resistivity;
-import simelectricity.essential.cable.TileCable;
 import simelectricity.essential.common.SEItemBlock;
 
 public class BlockIc2Cable extends BlockCable {
@@ -17,7 +18,7 @@ public class BlockIc2Cable extends BlockCable {
 				new String[]{"copper", "gold", "iron", "glass", "tin"},
 				new double[]{0.22, 0.22, 0.22, 0.22, 0.22},
 				new double[]{Resistivity.copper, Resistivity.gold, Resistivity.iron, Resistivity.glass, Resistivity.tin},
-				TileCable.class);
+				TileIc2Cable.class);
 		setHardness(0.2F);	
 	}
 	
---
+++ src/main/java/simelectricity/energybridge/ic2/cable/TileIc2Cable.java
@@ -0,0 +1,141 @@
+package simelectricity.energybridge.ic2.cable;
+
+import ic2.api.energy.event.EnergyTileLoadEvent;
+import ic2.api.energy.event.EnergyTileUnloadEvent;
+import ic2.api.energy.tile.IEnergySink;
+import ic2.api.energy.tile.IEnergySource;
+import ic2.api.tile.IEnergyStorage;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ForgeDirection;
+import simelectricity.essential.cable.TileCable;
+
+public class TileIc2Cable extends TileCable implements IEnergyStorage, IEnergySink, IEnergySource {
+
+	public boolean addedToEnergyNet;
+	
+    public boolean isAddedToEnergyNet()
+    {
+        return this.addedToEnergyNet;
+    }
+    
+    @Override
+    public void updateEntity()
+    {
+        super.updateEntity();
+        if (!worldObj.isRemote&!this.addedToEnergyNet)
+        {
+            //EnergyNet.getForWorld(this.worldObj).addTileEntity(this);
+            MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+            this.addedToEnergyNet = true;
+        }
+    }
+	
+    @Override
+    public void invalidate()
+    {
+        if (!worldObj.isRemote&this.addedToEnergyNet)
+        {
+            //EnergyNet.getForWorld(this.worldObj).removeTileEntity(this);
+            MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+            this.addedToEnergyNet = false;
+        }
+
+        super.invalidate();
+    }
+    
+	public TileIc2Cable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getStored() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setStored(int energy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int addEnergy(int amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOutput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getOutputEnergyUnitsPerTick() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isTeleporterCompatible(ForgeDirection side) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean acceptsEnergyFrom(TileEntity emitter, ForgeDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean emitsEnergyTo(TileEntity receiver, ForgeDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getOfferedEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void drawEnergy(double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSourceTier() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getDemandedEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSinkTier() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double injectEnergy(ForgeDirection directionFrom, double amount, double voltage) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
