8c8356c023a166c05c24a05e064acecfbf31acbe
MiniLight <MiniLightAR@Gmail.com>
2017-07-27 18:26:53 +0800

    e...

---
+++ src/main/java/simelectricity/essential/cable/Resistivity.java
@@ -0,0 +1,15 @@
+package simelectricity.essential.cable;
+
+public class Resistivity {
+	/*
+	 * https://en.wikipedia.org/wiki/Electrical_resistivity_and_conductivity
+	 */
+	final public static double graphene = 1.0;
+	final public static double silver = 1.59;
+	final public static double copper = 1.68;
+	final public static double gold = 2.44;
+	final public static double iron = 9.71;
+	final public static double tin = 10.9;
+	final public static double lead = 22.0;
+	final public static double glass = 1.0e20;
+}
